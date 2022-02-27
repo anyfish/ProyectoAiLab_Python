41bcf52081ad9f0f6d69aa200cc1bf8b8a6d4b82
Martín Farrera <60305182+anyfish@users.noreply.github.com>
2022-02-27 04:46:08 -0600

    interfaz

--- interfaz.py
+++ interfaz.py
@@ -1,19 +1,33 @@
 from tkinter import *
 from venv.GAME.Juego import *
 
-app = Tk()
-app.geometry("800x600")
-app.title("Aimon Game")
-app.configure(bg='gray')
-app.iconbitmap('pokeball.ico')
+def ventanaInicio():
+    vn = Tk()
+    vn.geometry("500x500")
+    vn.title("Aimon Game")
+    vn.configure(bg='gray')
+    vn.iconbitmap('egg.ico')
 
-labelNombre = Label(app, text='Ingresa tu Nombre')
-labelNombre.place(x=10, y=2)
-labelNombre.config(bg='gray')
+    labelTitulo = Label(vn, text='La aventura te espera en Aimon')
+    labelTitulo.place(x=10, y=2)
+    labelTitulo.config(bg='gray')
 
-entryNombre = Entry(app)
-entryNombre.place(x=10, y=25)
+    botonIniciarSesion = Button(vn, text='Iniciar Sesión', command=validarUsuario)
+    botonIniciarSesion.place(x=25, y=50)
 
-botonUsuarioNuevo = Button(app, text='Guardar', command=nuevoUsuario).place(x=10, y=50)
+    botonUsuarioNuevo = Button(vn, text='Nuevo Usuario', command=nuevoUsuario)
+    botonUsuarioNuevo.place(x=25, y=100)
 
-app.mainloop()
+    vn.mainloop()
+
+def ventanaIniciarSesion():
+    global vn
+    vn.geometry("500x500")
+    vn.title("Aimon Game")
+    vn.configure(bg='gray')
+    vn.iconbitmap('egg.ico')
+
+    entryNombre = Entry(vn)
+    entryNombre.place(x=10, y=25)
+
+    vn.mainloop()
